c18ba2afdde1b9eb16b5a32e50561351517f709b
Yusuke Matsushita <y.skm24t@gmail.com>
2023-04-10 05:01:36 +0900

    Remove sigT_wfo

--- nola/prelude.v
+++ nola/prelude.v
@@ -13,9 +13,3 @@ From Coq Require Export FunctionalExtensionality.
 
 Ltac fun_ext := apply functional_extensionality.
 Ltac fun_ext_dep := apply functional_extensionality_dep.
-
-(** ** Syntax sugar for [sigT] *)
-
-Notation "( x ,^ .. ,^ y ,^ z )" := (existT x .. (existT y z) ..).
-Notation "a .^1" := (projT1 a) (at level 2).
-Notation "a .^2" := (projT2 a) (at level 2).
