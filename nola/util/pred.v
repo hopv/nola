ec80eeef36078c55e1f15a5e1b26a04fc1e2928e
Yusuke Matsushita <y.skm24t@gmail.com>
2023-04-10 13:41:33 +0900

    Update Mono1 to use Proper

--- nola/util/pred.v
+++ nola/util/pred.v
@@ -17,7 +17,10 @@ Definition and1 {A} (φ ψ : A → Prop) a : Prop := φ a ∧ ψ a.
 Infix "∧1" := and1 (at level 40, left associativity).
 Notation "(∧1)" := and1 (only parsing).
 
-Definition impl1 {A} (φ ψ : A → Prop) : Prop := ∀ a, φ a → ψ a.
+Infix "→0" := impl (at level 99, right associativity).
+Notation "(→0)" := impl (only parsing).
+
+Definition impl1 {A} : relation (A → Prop) := pointwise_relation A impl.
 Infix "→1" := impl1 (at level 99, right associativity).
 Notation "(→1)" := impl1 (only parsing).
 
@@ -27,11 +30,27 @@ Definition por {A} (p : (A → Prop) → A → Prop) (φ : A → Prop) : A → P
 Definition pand {A} (p : (A → Prop) → A → Prop) (φ : A → Prop) : A → Prop :=
   p φ ∧1 φ.
 
-(** Monotonicity *)
+(** ** Monotonicity *)
 
-Class Mono1 A (p : (A → Prop) → A → Prop) : Prop := {
-  mono1 : ∀{φ ψ} , (φ →1 ψ) → p φ →1 p ψ;
-}.
+Notation Mono1 A := (Proper (@impl1 A ==> @impl1 A)).
+
+Lemma go_mono1 A p `(monop : Mono1 A p) φ ψ a : (φ →1 ψ) → p φ a → p ψ a.
+Proof. by move=> /monop->. Qed.
+
+(** Simplifies the goal [p φ a → p ψ a] into [φ →1 ψ] *)
+Ltac go_mono1 := apply go_mono1; [apply _|].
+(** Simplifies the goal [p ψ a] into [φ →1 ψ] consuming [x : p φ a] *)
+Tactic Notation "go_mono1" "with" constr(x) := move: x; go_mono1.
+
+(** Proper instances for [Mono1] *)
+
+Global Instance Mono1_impl `(Mono1 A p) :
+  Proper ((→1) ==> (=) ==> (→0)) p.
+Proof. move=> φ ψ /=? a _ <- +. by go_mono1. Qed.
+
+Global Instance Mono1_flip_impl `(Mono1 A p) :
+  Proper ((→1) --> (=) ==> flip (→0)) p.
+Proof. move=> ψ φ /=? a _ <- +. by go_mono1. Qed.
 
 (** ** [pnu]: Parameterized greatest fixpoint *)
 
@@ -47,25 +66,21 @@ Section pnu.
 
   (** Fold [pnu] *)
   Lemma pnu_fold φ a : F (por pnu φ) a → pnu φ a.
-  Proof.
-    move=> Fpnuor. split. exists (por pnu φ). split; by [move=> >|].
-  Qed.
+  Proof. move=> ?. split. exists (por pnu φ). split; by [move=> >|]. Qed.
 
   (** Unfold [pnu] *)
-  Lemma pnu_unfold `{Mono1 _ F} φ a : pnu φ a → F (por pnu φ) a.
-  Proof.
-    move=> /pnu_out[pnuor[pnuorto ?]]. by apply (mono1 pnuorto).
-  Qed.
+  Lemma pnu_unfold `(Mono1 _ F) φ a : pnu φ a → F (por pnu φ) a.
+  Proof. move=> /pnu_out[_[->]]. done. Qed.
 
   (** Monotonicity of [pnu] *)
   Global Instance pnu_mono : Mono1 _ pnu.
   Proof.
-    split=> φ ψ φψ. cofix FIX=> a /pnu_out[pnuor[pnuorto ?]]. split.
+    move=> φ ψ φψ. cofix FIX=> a /pnu_out[pnuor[pnuorto ?]]. split.
     exists pnuor. split; [|done]=> ? /pnuorto[/FIX|/φψ]; by [left|right].
   Qed.
 
   (** Accumulate coinductive hypotheses *)
-  Lemma pnu_acc `{Mono1 _ F} φ ψ : (ψ →1 pnu (φ ∨1 ψ)) → ψ →1 pnu φ.
+  Lemma pnu_acc `(Mono1 _ F) φ ψ : (ψ →1 pnu (φ ∨1 ψ)) → ψ →1 pnu φ.
   Proof.
     move=> topnuφψ=> a /topnuφψ +. move: a. cofix FIX=> a /pnu_unfold ?.
     split. exists (por pnu (φ ∨1 ψ)). split; [|done]. clear dependent a.
@@ -76,17 +91,15 @@ Section pnu.
   Definition nu : A → Prop := pnu False1.
 
   (** Fold [nu] *)
-  Lemma nu_fold `{Mono1 _ F} a : F nu a → nu a.
+  Lemma nu_fold `(Mono1 _ F) a : F nu a → nu a.
   Proof.
-    move=> Fnua. apply pnu_fold. eapply mono1; [|exact Fnua].
-    clear dependent a=> a nua. left. by eapply mono1; [|exact nua]=> +.
+    move=> Fnua. apply pnu_fold. go_mono1 with Fnua. clear a=> a nua. left.
+    by go_mono1 with nua.
   Qed.
 
   (** Unfold [nu] *)
-  Lemma nu_unfold `{Mono1 _ F} a : nu a → F nu a.
-  Proof.
-    move=> /pnu_unfold FnuFa. eapply mono1; [|exact FnuFa]. move=> _ [//|[]].
-  Qed.
+  Lemma nu_unfold `(Mono1 _ F) a : nu a → F nu a.
+  Proof. move/pnu_unfold. go_mono1. move=> _ [//|[]]. Qed.
 End pnu.
 
 (** ** [pmu]: Parameterized least fixpoint *)
@@ -103,46 +116,39 @@ Section pmu.
 
   (** Fold [pmu] *)
   Lemma pmu_fold φ a : F (pand pmu φ) a → pmu φ a.
-  Proof.
-    move=> Fpmuand. split. exists (pand pmu φ). split; by [move=> >|].
-  Qed.
+  Proof. move=> ?. split. exists (pand pmu φ). split; by [move=> >|]. Qed.
 
   (** Unfold [pmu] *)
-  Lemma pmu_unfold `{Mono1 _ F} φ a : pmu φ a → F (pand pmu φ) a.
-  Proof.
-    move=> /pmu_out[pmuand[pmuandto ?]]. by apply (mono1 pmuandto).
-  Qed.
+  Lemma pmu_unfold `(Mono1 _ F) φ a : pmu φ a → F (pand pmu φ) a.
+  Proof. move=> /pmu_out[_[->]]. done. Qed.
 
   (** Monotonicity of [pmu] *)
   Global Instance pmu_mono : Mono1 _ pmu.
   Proof.
-    split=> φ ψ φψ +. fix FIX 2=> a /pmu_out[pmuand[pmuandto ?]]. split.
+    move=> φ ψ φψ ++. fix FIX 2=> a /pmu_out[pmuand[pmuandto ?]]. split.
     exists pmuand. split; [|done]=> ? /pmuandto[/FIX+ /φψ]//.
   Qed.
 
   (** Accumulate inductive hypotheses *)
-  Lemma pmu_acc `{Mono1 _ F} φ ψ : (pmu (φ ∧1 ψ) →1 ψ) → pmu φ →1 ψ.
+  Lemma pmu_acc `(Mono1 _ F) φ ψ : (pmu (φ ∧1 ψ) →1 ψ) → pmu φ →1 ψ.
   Proof.
     move=> topmuφψ a pmuφa. apply topmuφψ. move: a pmuφa. fix FIX 2=> a.
     move=> [[pmuand[pmuandto Fpmuanda]]]. apply pmu_fold.
-    eapply mono1; [|exact Fpmuanda]. clear dependent a=> a pmuanda.
-    move: (pmuandto _ pmuanda)=> [/FIX pmuφψa φa]. split; [done|].
-    split; [done|]. by apply topmuφψ.
+    go_mono1 with Fpmuanda. clear a=> a /pmuandto[/FIX pmuφψa φa].
+    split; [done|]. split; [done|]. by apply topmuφψ.
   Qed.
 
   (** [mu]: Non-parameterized least fixpoint *)
   Definition mu : A → Prop := pmu True1.
 
   (** Fold [mu] *)
-  Lemma mu_fold `{Mono1 _ F} a : F mu a → mu a.
+  Lemma mu_fold `(Mono1 _ F) a : F mu a → mu a.
   Proof.
-    move=> Fmua. apply pmu_fold. eapply mono1; [|exact Fmua].
-    clear dependent a=> a mua. split; [|done]. by eapply mono1; [|exact mua].
+    move=> Fmua. apply pmu_fold. go_mono1 with Fmua. clear a=> a mua.
+    split; [|done]. by go_mono1 with mua.
   Qed.
 
   (** Unfold [mu] *)
-  Lemma mu_unfold `{Mono1 _ F} a : mu a → F mu a.
-  Proof.
-    move=> /pmu_unfold FmuFa. eapply mono1; [|exact FmuFa]. by move=> ?[??].
-  Qed.
+  Lemma mu_unfold `(Mono1 _ F) a : mu a → F mu a.
+  Proof. move/pmu_unfold. go_mono1. by move=> ?[??]. Qed.
 End pmu.
