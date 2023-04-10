5228973d1567757a03b4a0213bb5d6ff2de49063
Yusuke Matsushita <y.skm24t@gmail.com>
2023-04-10 14:25:33 +0900

    Add subrelation utility

--- nola/prelude.v
+++ nola/prelude.v
@@ -13,3 +13,16 @@ From Coq Require Export FunctionalExtensionality.
 
 Ltac fun_ext := apply functional_extensionality.
 Ltac fun_ext_dep := apply functional_extensionality_dep.
+
+(** ** Subrelation *)
+
+Arguments subrelation {A} (R R')%signature.
+
+Global Instance eq_subrel_respect {A B}
+  `(subR : subrelation A R (=)) `(subR' : subrelation B (=) R') :
+  subrelation (=) (R ==> R').
+Proof. move=> f _ <- a _ /subR<-. by apply subR'. Qed.
+
+Global Instance pointwise_subrel_eq {A B} `(subR : subrelation B R (=)) :
+  subrelation (pointwise_relation A R) (=).
+Proof. move=> f g Rfg. fun_ext=> a. by apply subR. Qed.
