ee630c4ad87de732e5621146b16e2ce321c2ee09
Yusuke Matsushita <y.skm24t@gmail.com>
2023-04-19 01:39:57 +0900

    Add lemmas for tc

--- nola/util/rel.v
+++ nola/util/rel.v
@@ -2,6 +2,41 @@
 
 From nola Require Import prelude util.pred.
 
+(** ** Transitive closure *)
+
+Section tc_facts.
+  Context {A : Type}.
+  Implicit Type R : A → A → Prop.
+
+  (** [tc']: [tc] defined backward *)
+  Definition tc' R : A → A → Prop := flip (tc (flip R)).
+  Arguments tc' R a b /.
+
+  (** [tc] is equivalent to [tc'] *)
+
+  Local Lemma tc'_to_tc {R a b} : tc' R a b → tc R a b.
+  Proof.
+    elim {a b}=>/=; [move=> >; by apply tc_once|]=> c b a Rbc _ tcRab.
+    by eapply tc_r.
+  Qed.
+
+  Lemma tc_tc' {R a b} : tc R a b ↔ tc' R a b.
+  Proof. split; apply tc'_to_tc. Qed.
+
+  (** Decompose [tc] from the left *)
+  Lemma tc_inv_l {R a b} : tc R a b → R a b ∨ ∃ c, R a c ∧ tc R c b.
+  Proof.
+    case {a b}; [by left|]=> a c b Rac tcRcb. right. by exists c.
+  Qed.
+
+  (** Decompose [tc] from the right *)
+  Lemma tc_inv_r {R a b} : tc R a b → R a b ∨ ∃ c, tc R a c ∧ R c b.
+  Proof.
+    case/tc_tc' {a b}; [by left|]=>/= b c a Rcb /tc_tc'/= tcRac. right.
+    by exists c.
+  Qed.
+End tc_facts.
+
 (** ** Facts about well-founded relations *)
 Section wf_facts.
   (** Take any well-founded relation [R] *)
@@ -19,5 +54,12 @@ Section wf_facts.
   Proof.
     move=> a b. move: a (wfR a) b. fix FIX 2=> a Acca b ab ba.
     by apply (FIX b (Acc_inv Acca ba) a).
+  Qed.
+
+  (** [tc R] is well-founded *)
+  Lemma tc_wf : wf (tc R).
+  Proof.
+    move=> a. elim: {a}(wfR a)=> a _ IH. apply Acc_intro=> b /tc_inv_r.
+    case; [by apply IH|]=> [[c [tcRbc /IH Accc]]]. exact (Acc_inv Accc tcRbc).
   Qed.
 End wf_facts.
