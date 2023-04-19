efd3c9dbc85be79421d65469674120ce29c32a83
Yusuke Matsushita <y.skm24t@gmail.com>
2023-04-19 21:12:41 +0900

    Argument, layout and comment tweak

--- nola/logic/prop.v
+++ nola/logic/prop.v
@@ -3,9 +3,9 @@
 From nola Require Import prelude util.tlist util.wft.
 From iris.bi Require Import notation.
 
-(** ** [nsx]: Syntactic extension *)
+(** ** [nsx]: Syntactic extension for [nProp] *)
 
-(** [nsxs]: Syntactic extension for [nPropS] and [nPropL] *)
+(** [nsxs]: Syntactic extension for both [nPropS] and [nPropL] *)
 Structure nsxs : Type := Nsxs {
   (** Data *)
   nsxs_data : Type;
@@ -29,7 +29,7 @@ Structure nsxl : Type := Nsxl {
   nsxl_parcl : nsxl_data → Type;
 }.
 
-(** [nsx]: Combination of [nsxs] and [nsxl] *)
+(** [nsx]: Syntactic extension for [nProp], combination of [nsxs] and [nsxl] *)
 Record nsx : Type := Nsx { nsx_s :> nsxs; nsx_l :> nsxl; }.
 
 (** ** [nctx]: Context of [nProp] *)
@@ -45,54 +45,63 @@ Definition nvar (Γ : nctx) : Type := [+] A ∈ Γ, A.
 (** ** [nProp]: Nola syntactic proposition *)
 
 (** [nsort]: Sort of [nProp], [nS] or [nL] *)
-Variant nsort : Set := nS | nL.
+Variant nsort : Set := (** small *) nS | (** large *) nL.
 
 (** [nProp]: Nola syntactic proposition
+
   Its universe level is strictly higher than those of [Γ : nctx],
   the index [I : wft] of the judgment derivability,
-  and the domain [A : Type] of [np_forall]/[np_exists] *)
+  and the domain [A : Type] of [np_forall]/[np_exists]
 
-Inductive nProp (Ξ : nsx) : nsort → tlist Type → tlist Type → Type :=
-| (** Inner variable *) np_var {σ Γ Δ} : nvar Δ → nProp Ξ σ Γ Δ
+  We make [Ξ] implicit for each constructor;
+  we later make it explicit for [nProp] *)
+
+Inductive nProp {Ξ : nsx} : nsort → tlist Type → tlist Type → Type :=
+| (** Inner variable *) np_var {σ Γ Δ} : nvar Δ → nProp σ Γ Δ
 | (** Outer variable, available only in [nPropL] *)
-  np_ovar {Γ Δ} : nvar Γ → nProp Ξ nL Γ Δ
+  np_ovar {Γ Δ} : nvar Γ → nProp nL Γ Δ
 | (** Judgment derivability *)
-  np_deriv {σ Γ Δ} (I : wft) : I → nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
-| (** Empty proposition *) np_emp {σ Γ Δ} : nProp Ξ σ Γ Δ
-| (** Pure proposition *) np_pure {σ Γ Δ} : Prop → nProp Ξ σ Γ Δ
-| (** Conjunction *) np_and {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
-| (** Disjunction *) np_or {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
-| (** Implication *) np_impl {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
+  np_deriv {σ Γ Δ} (I : wft) : I → nProp σ Γ Δ → nProp σ Γ Δ → nProp σ Γ Δ
+| (** Empty proposition *) np_emp {σ Γ Δ} : nProp σ Γ Δ
+| (** Pure proposition *) np_pure {σ Γ Δ} : Prop → nProp σ Γ Δ
+| (** Conjunction *) np_and {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ → nProp σ Γ Δ
+| (** Disjunction *) np_or {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ → nProp σ Γ Δ
+| (** Implication *) np_impl {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ → nProp σ Γ Δ
 | (** Separating conjunction *)
-  np_sep {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
-| (** Magic wand *) np_wand {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
+  np_sep {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ → nProp σ Γ Δ
+| (** Magic wand *) np_wand {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ → nProp σ Γ Δ
 | (** Universal quantification *)
-  np_forall {σ Γ Δ} (A : Type) : (A → nProp Ξ σ Γ Δ) → nProp Ξ σ Γ Δ
+  np_forall {σ Γ Δ} (A : Type) : (A → nProp σ Γ Δ) → nProp σ Γ Δ
 | (** Existential quantification *)
-  np_exist {σ Γ Δ} (A : Type) : (A → nProp Ξ σ Γ Δ) → nProp Ξ σ Γ Δ
+  np_exist {σ Γ Δ} (A : Type) : (A → nProp σ Γ Δ) → nProp σ Γ Δ
 | (** Second-order universal quantification *)
-  np_so_forall {σ Γ Δ} (A : Type) : nProp Ξ σ (A ^:: Γ) Δ → nProp Ξ σ Γ Δ
+  np_so_forall {σ Γ Δ} (A : Type) : nProp σ (A ^:: Γ) Δ → nProp σ Γ Δ
 | (** Second-order existential quantification *)
-  np_so_exist {σ Γ Δ} (A : Type) : nProp Ξ σ (A ^:: Γ) Δ → nProp Ξ σ Γ Δ
+  np_so_exist {σ Γ Δ} (A : Type) : nProp σ (A ^:: Γ) Δ → nProp σ Γ Δ
 | (** Persistence modality *)
-  np_persistently {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
-| (** Plainly modality *) np_plainly {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
+  np_persistently {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ
+| (** Plainly modality *) np_plainly {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ
 | (** Later modality
 
     Because it is contractive, its argument proposition can be in [nPropL]
-    and have outer variables flushed *)
-  np_later {σ} Γ {Δ} : nProp Ξ nL ^[] (Γ ^++ Δ) → nProp Ξ σ Γ Δ
-| (** Basic update modality *) np_bupd {σ Γ Δ} : nProp Ξ σ Γ Δ → nProp Ξ σ Γ Δ
+    and have outer variables flushed
+
+    For the users to aid type inference around [^++], we expose [Γ]
+    as the explicit parameter (the same applies to [np_ex] and [np_exl]) *)
+  np_later {σ} Γ {Δ} : nProp nL ^[] (Γ ^++ Δ) → nProp σ Γ Δ
+| (** Basic update modality *) np_bupd {σ Γ Δ} : nProp σ Γ Δ → nProp σ Γ Δ
 | (** Extension by [Ξ.(nsx_s)] *)
   np_ex {σ} Γ {Δ} d :
-    (Ξ.(nsxs_paru) d → nProp Ξ σ Γ Δ) →
-    (Ξ.(nsxs_parcs) d → nProp Ξ nS ^[] (Γ ^++ Δ)) →
-    (Ξ.(nsxs_parcl) d → nProp Ξ nL ^[] (Γ ^++ Δ)) → nProp Ξ σ Γ Δ
+    (Ξ.(nsxs_paru) d → nProp σ Γ Δ) →
+    (Ξ.(nsxs_parcs) d → nProp nS ^[] (Γ ^++ Δ)) →
+    (Ξ.(nsxs_parcl) d → nProp nL ^[] (Γ ^++ Δ)) → nProp σ Γ Δ
 | (** Extension by [Ξ.(nsx_l)] *)
   np_exl Γ {Δ} d :
-    (Ξ.(nsxs_paru) d → nProp Ξ nL Γ Δ) →
-    (Ξ.(nsxs_parcs) d → nProp Ξ nS ^[] (Γ ^++ Δ)) →
-    (Ξ.(nsxs_parcl) d → nProp Ξ nL ^[] (Γ ^++ Δ)) → nProp Ξ nL Γ Δ.
+    (Ξ.(nsxs_paru) d → nProp nL Γ Δ) →
+    (Ξ.(nsxs_parcs) d → nProp nS ^[] (Γ ^++ Δ)) →
+    (Ξ.(nsxs_parcl) d → nProp nL ^[] (Γ ^++ Δ)) → nProp nL Γ Δ.
+
+Arguments nProp Ξ σ Γ Δ : clear implicits.
 
 (** Notations for large or small [nProp] *)
 Notation nPropL Ξ := (nProp Ξ nL).
