adc346d827bd29eaf93e2f52225dcf2586c1af2d
Yusuke Matsushita <y.skm24t@gmail.com>
2023-04-22 04:31:18 +0900

    Minor tweak on nctx

--- nola/logic/prop.v
+++ nola/logic/prop.v
@@ -21,11 +21,15 @@ Record nctx : Type := Nctx {
   nctx_l : nectx;
 }.
 
+(** Notations for [nctx] *)
+
 Declare Scope nctx_scope.
 Delimit Scope nctx_scope with nctx.
 Bind Scope nctx_scope with nctx.
+
 Notation "( Γₒₛ , Γₛ ; Γₒₗ , Γₗ )" := (Nctx Γₒₛ Γₛ Γₒₗ Γₗ) : nctx_scope.
 Notation "( Γₛ ; Γₗ )" := (Nctx ^[] Γₛ ^[] Γₗ) : nctx_scope.
+Notation "( Γₒₛ , ; Γₒₗ , )" := (Nctx Γₒₛ ^[] Γₒₗ ^[]) : nctx_scope.
 Notation "( Γₒₛ , Γₛ ; )" := (Nctx Γₒₛ Γₛ ^[] ^[]) : nctx_scope.
 Notation "( Γₛ ; )" := (Nctx ^[] Γₛ ^[] ^[]) : nctx_scope.
 Notation "( ; Γₒₗ , Γₗ )" := (Nctx ^[] ^[] Γₒₗ Γₗ)
