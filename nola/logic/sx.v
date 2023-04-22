adb9dc4eff541c8c712ed4e117f854b901834121
Yusuke Matsushita <y.skm24t@gmail.com>
2023-04-22 19:40:05 +0900

    Update logic.sx

--- nola/logic/sx.v
+++ nola/logic/sx.v
@@ -22,9 +22,31 @@ Arguments nesx_pu {Ξ} d : rename.
 Arguments nesx_pns {Ξ} d : rename.
 Arguments nesx_pnl {Ξ} d : rename.
 
-(** ** [Nsubesx]: Inclusion between [nesx]s *)
+(** ** [nesxsum]: Sum of [nesx]s *)
 
-Class Nsubesx (Ξ' Ξ : nesx) := {
+Section nesxsum.
+  Context {I : Type} {Ξs : I → nesx}.
+
+  #[projections(primitive)]
+  Record nesxsum := Nesxsum {
+    (** Index *)
+    nesxsum_i : I;
+    (** Proposition data *)
+    nesxsum_d : Ξs nesxsum_i;
+  }.
+  Add Printing Constructor nesxsum.
+
+  Canonical nesxsum_nesx := Nesx nesxsum
+    (λ '(Nesxsum i d), (Ξs i).(nesx_pu) d)
+    (λ '(Nesxsum i d), (Ξs i).(nesx_pns) d)
+    (λ '(Nesxsum i d), (Ξs i).(nesx_pnl) d).
+End nesxsum.
+
+Arguments nesxsum {I} Ξs.
+
+(** ** [nsubesx]: Inclusion between [nesx]s *)
+
+Class nsubesx {Ξ' Ξ : nesx} : Type := Nsubesx {
   (** Inclusion between [nesx_d] *)
   nsubesx_d : Ξ'.(nesx_d) → Ξ.(nesx_d);
   (** Inclusion between [nesx_pu] *)
@@ -34,14 +56,27 @@ Class Nsubesx (Ξ' Ξ : nesx) := {
   (** Inclusion between [nesx_pnl] *)
   nsubesx_pnl d : Ξ.(nesx_pnl) (nsubesx_d d) → Ξ'.(nesx_pnl) d;
 }.
-Notation "Ξ' ⊑esx Ξ" := (Nsubesx Ξ' Ξ) (at level 70, no associativity)
+Add Printing Constructor nsubesx.
+Arguments nsubesx Ξ' Ξ : clear implicits.
+Arguments Nsubesx {Ξ' Ξ}.
+
+Notation "Ξ' ⊑esx Ξ" := (nsubesx Ξ' Ξ) (at level 70, no associativity)
   : nola_scope.
 
-(** [⊑esx] is reflexive *)
-#[export] Instance Nsubesx_refl {Ξ} : Ξ ⊑esx Ξ := {
-  nsubesx_d := id;
-  nsubesx_pu _ := id; nsubesx_pns _ := id; nsubesx_pnl _ := id;
-}.
+(** Identity [⊑esx] *)
+#[export] Instance nsubesx_id {Ξ} : Ξ ⊑esx Ξ :=
+  Nsubesx id (λ _, id) (λ _, id) (λ _, id).
+
+(** Compose [⊑esx]s *)
+Definition nsubesx_comp {Ξ'' Ξ' Ξ} (f : Ξ'' ⊑esx Ξ') (g : Ξ' ⊑esx Ξ)
+  : Ξ'' ⊑esx Ξ := Nsubesx (g.(nsubesx_d) ∘ f.(nsubesx_d))
+  (λ d, f.(nsubesx_pu) d ∘ g.(nsubesx_pu) (f.(nsubesx_d) d))
+  (λ d, f.(nsubesx_pns) d ∘ g.(nsubesx_pns) (f.(nsubesx_d) d))
+  (λ d, f.(nsubesx_pnl) d ∘ g.(nsubesx_pnl) (f.(nsubesx_d) d)).
+
+(** Inclusion into [nesxsum] *)
+Definition nsubesx_nessum {I Ξs} (i : I) : Ξs i ⊑esx nesxsum Ξs :=
+  Nsubesx (Nesxsum i) (λ _, id) (λ _, id) (λ _, id).
 
 (** ** [nsx]: Syntactic extension for [nProp] *)
 
