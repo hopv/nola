ccb6fb7aae7302574abeec8821a8e5b4b466c0db
Yusuke Matsushita <y.skm24t@gmail.com>
2023-05-19 11:54:40 +0900

    Update util.funext

--- nola/util/funext.v
+++ nola/util/funext.v
@@ -3,18 +3,18 @@
 From nola Require Export prelude.
 From Coq Require Export FunctionalExtensionality.
 
-(** ** Tactics *)
+(** ** Apply functional extensionality, without introducing a name *)
+Ltac funext :=
+  apply functional_extensionality || apply functional_extensionality_dep.
 
-Ltac funext := apply functional_extensionality.
-Ltac funext_dep := apply functional_extensionality_dep.
-
-(** ** Subrelation *)
-
+(** ** Make [(=)] a subrelation of [==>] *)
 #[export] Instance eq_subrel_respect {A B}
   `{subR : subrelation A R (=)} `{subR' : subrelation B (=) R'} :
   subrelation (=) (R ==> R')%signature.
 Proof. move=> f _ <- a _ /subR<-. by apply subR'. Qed.
 
+(** ** Make [pointwise_relation] a subrelation of [(=)],
+  using functional extensionality *)
 #[export] Instance pointwise_subrel_eq {A B} `{subR : subrelation B R (=)} :
   subrelation (pointwise_relation A R) (=).
 Proof. move=> f g Rfg. funext=> a. by apply subR. Qed.
