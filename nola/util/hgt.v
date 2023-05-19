b58fbb77496df33c31f91660eb6581f736e04c50
Yusuke Matsushita <y.skm24t@gmail.com>
2023-05-19 12:19:33 +0900

    Introduce util.hgt

---
+++ nola/util/hgt.v
@@ -0,0 +1,40 @@
+(** *  General height of a tree *)
+
+From nola Require Export prelude.
+Import EqNotations.
+
+(** [hgt]: General height of a tree *)
+Inductive hgt : Type := hgt_all {
+  hgt_dom : Type;
+  hgt_kids :> hgt_dom → hgt;
+}.
+Arguments hgt_all {A} hs : rename.
+Add Printing Constructor hgt.
+
+Definition hgt_0 : hgt := hgt_all (λ e : Empty_set, match e with end).
+Definition hgt_1 (h : hgt) : hgt := hgt_all (λ _ : unit, h).
+Definition hgt_2 (h h' : hgt) : hgt :=
+  hgt_all (λ b : bool, if b then h else h').
+
+(** [hAcc]: Accessibility on [hgt] *)
+Inductive hAcc (h : hgt) : Prop := HAcc {
+  hateq :> ∀ h' a, h' = h a → hAcc h'
+}.
+Arguments HAcc {_} _.
+Add Printing Constructor hAcc.
+
+(** Destruct [hAcc] in a simple way *)
+Notation hat H a := (H _ a eq_refl).
+Infix "‼ʰ" := hat (at level 20, no associativity) : nola_scope.
+
+(** [hwf]: [hAcc h] for all [h : hgt] *)
+Fixpoint hwf {h} : hAcc h := HAcc (λ _ _ eq, rew eq_sym eq in hwf).
+
+(** Under functional extensionality *)
+From nola Require Import util.funext.
+
+(** [hAcc] is proof-irrelevant *)
+Lemma hAcc_irrel {h} (H H' : hAcc h) : H = H'.
+Proof.
+  move: h H H'. fix FIX 1=> ?[?][?]. f_equal. do 3 funext=> ?. by subst.
+Qed.
