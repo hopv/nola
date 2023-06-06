e9205649bc0ec5efa960a7cd6bd9223d06fb593e
Yusuke Matsushita <y.skm24t@gmail.com>
2023-06-07 01:15:20 +0900

    Move lemmas on sintp to logic.facts

--- nola/examples/logic/facts.v
+++ nola/examples/logic/facts.v
@@ -55,4 +55,88 @@ Section facts.
   Proof. by rewrite/= rew_eq_hwf. Qed.
   Lemma nintp_subus {s P} : ⟦ !ᵘˢ P ⟧(s) ⊣⊢ ⟦ ↑ˡ P ⟧(s).
   Proof. by rewrite/= nintpS_nintp_nlarge. Qed.
+
+  Context `{!nsintpy Σ σ}.
+  Implicit Type (i j : nat) (P Q : nPropL (;ᵞ)).
+
+  (** Make connectives go inside the strong interpretation *)
+  Lemma sintpy_and {i s P Q} :
+    ⸨ P ⸩(σ s, i) ∧ ⸨ Q ⸩(σ s, i) -∗ ⸨ P ∧ Q ⸩(σ s, i).
+  Proof.
+    iIntros "PQ". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
+    iSplit; iApply "to"; [iDestruct "PQ" as "[$_]"|iDestruct "PQ" as "[_$]"].
+  Qed.
+  Lemma sintpy_or {i s P Q} :
+    ⸨ P ⸩(σ s, i) ∨ ⸨ Q ⸩(σ s, i) -∗ ⸨ P ∨ Q ⸩(σ s, i).
+  Proof.
+    iIntros "[?|?]"; iApply sintpy_byintp; iIntros (? _) "/= #to _ _";
+      [iLeft|iRight]; by iApply "to".
+  Qed.
+  Lemma sintpy_forall {i s A} {Φ : A → nPropL (;ᵞ)} :
+    (∀ a, ⸨ Φ a ⸩(σ s, i)) -∗ ⸨ ∀' Φ ⸩(σ s, i).
+  Proof.
+    iIntros "Φ". iApply sintpy_byintp. iIntros (? _) "/= #to _ _". iIntros (a).
+    iApply "to". iApply "Φ".
+  Qed.
+  Lemma sintpy_exist {i s A} {Φ : A → nPropL (;ᵞ)} :
+    (∃ a, ⸨ Φ a ⸩(σ s, i)) -∗ ⸨ ∃' Φ ⸩(σ s, i).
+  Proof.
+    iDestruct 1 as (a) "Φ". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
+    iExists a. iApply "to". iApply "Φ".
+  Qed.
+  Lemma sintpy_sep {i s P Q} :
+    ⸨ P ⸩(σ s, i) ∗ ⸨ Q ⸩(σ s, i) -∗ ⸨ P ∗ Q ⸩(σ s, i).
+  Proof.
+    iIntros "[P Q]". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
+    iSplitL "P"; by iApply "to".
+  Qed.
+  Lemma sintpy_persistently {i s P} : □ ⸨ P ⸩(σ s, i) -∗ ⸨ □ P ⸩(σ s, i).
+  Proof.
+    iIntros "#P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _ !#".
+    by iApply "to".
+  Qed.
+  Lemma sintpy_bupd {i s P} : (|==> ⸨ P ⸩(σ s, i)) -∗ ⸨ |==> P ⸩(σ s, i).
+  Proof.
+    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
+    by iApply "to".
+  Qed.
+  Lemma sintpy_fupd {i s E E' P} :
+    (|={E,E'}=> ⸨ P ⸩(σ s, i)) -∗ ⸨ |={E,E'}=> P ⸩(σ s, i).
+  Proof.
+    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
+    by iApply "to".
+  Qed.
+  Lemma sintpy_later {i s P} : ▷ ⸨ P ⸩(σ s, i) -∗ ⸨ ▷{nil} P ⸩(σ s, i).
+  Proof.
+    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
+    rewrite nintp_fp_nintp. by iApply "to".
+  Qed.
+  Lemma sintpy_n_forall {i s V} {P : nPropL ([V];ᵞ )} :
+    (∀ Φ, ⸨ P /: Φ ⸩(σ s, i)) -∗ ⸨ ∀: V, P ⸩(σ s, i).
+  Proof.
+    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _". iIntros (Ψ).
+    rewrite rew_eq_hwf. iApply "to". iApply "P".
+  Qed.
+  Lemma sintpy_n_exist {i s V} {P : nPropL ([V];ᵞ )} :
+    (∃ Φ, ⸨ P /: Φ ⸩(σ s, i)) -∗ ⸨ ∃: V, P ⸩(σ s, i).
+  Proof.
+    iDestruct 1 as (Φ) "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
+    iExists Φ. rewrite rew_eq_hwf. iApply "to". iApply "P".
+  Qed.
+
+  (** Introduce [○(i)] *)
+  Lemma sintpy_indir_intro {i j s P} :
+    ⸨ P ⸩(σ $∨ˢ s, i) -∗ ⸨ ○{nil}(i) P ⸩(σ s, j).
+  Proof.
+    iIntros "P". iApply sintpy_byintp. iIntros (σ' _) "/= _ #toσ' _".
+    by iApply "toσ'".
+  Qed.
+  (** Eliminate [○(i)] under a strong interpration of level [j > i] *)
+  Lemma sintpy_indir_elim {i j s P} :
+    i < j → ⸨ ○{nil}(i) P ⸩(σ s, j) -∗ ⸨ P ⸩(σ s, j).
+  Proof.
+    move=> ij. iIntros "○P". iApply sintpy_byintp.
+    iIntros (σ' _) "/= #to #toσ' #σ'to". iDestruct ("to" with "○P") as "/= ○P".
+    by iApply "σ'to".
+  Qed.
 End facts.
--- nola/examples/logic/sintp.v
+++ nola/examples/logic/sintp.v
@@ -12,10 +12,10 @@ Definition nintpsi Σ `{!nintpGS Σ} : intpsi :=
 Notation nsintpy Σ := (sintpy (nintpsi Σ)).
 Notation nsintp Σ := (sintp (nintpsi Σ)).
 
-Implicit Type (i j : nat) (P Q : nPropL (;ᵞ)).
+Implicit Type P : nPropL (;ᵞ).
 
-(** ** Lemmas about [nsintpy] *)
-Section lemmas.
+(** ** On [nsintpy] *)
+Section nsintpy.
   Context `{!nintpGS Σ, !nsintpy Σ σ}.
 
   (** [σ s] is monotone over the level *)
@@ -24,85 +24,4 @@ Section lemmas.
     move/Nat.lt_eq_cases=> [ij|<-]; [|by iIntros]. iIntros "Pi".
     iApply sintpy_map_lev; by [|iIntros|iLeft].
   Qed.
-
-  (** Make connectives go inside the strong interpretation *)
-  Lemma sintpy_and {i s P Q} :
-    ⸨ P ⸩(σ s, i) ∧ ⸨ Q ⸩(σ s, i) -∗ ⸨ P ∧ Q ⸩(σ s, i).
-  Proof.
-    iIntros "PQ". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
-    iSplit; iApply "to"; [iDestruct "PQ" as "[$_]"|iDestruct "PQ" as "[_$]"].
-  Qed.
-  Lemma sintpy_or {i s P Q} :
-    ⸨ P ⸩(σ s, i) ∨ ⸨ Q ⸩(σ s, i) -∗ ⸨ P ∨ Q ⸩(σ s, i).
-  Proof.
-    iIntros "[?|?]"; iApply sintpy_byintp; iIntros (? _) "/= #to _ _";
-      [iLeft|iRight]; by iApply "to".
-  Qed.
-  Lemma sintpy_forall {i s A} {Φ : A → nPropL (;ᵞ)} :
-    (∀ a, ⸨ Φ a ⸩(σ s, i)) -∗ ⸨ ∀' Φ ⸩(σ s, i).
-  Proof.
-    iIntros "Φ". iApply sintpy_byintp. iIntros (? _) "/= #to _ _". iIntros (a).
-    iApply "to". iApply "Φ".
-  Qed.
-  Lemma sintpy_exist {i s A} {Φ : A → nPropL (;ᵞ)} :
-    (∃ a, ⸨ Φ a ⸩(σ s, i)) -∗ ⸨ ∃' Φ ⸩(σ s, i).
-  Proof.
-    iDestruct 1 as (a) "Φ". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
-    iExists a. iApply "to". iApply "Φ".
-  Qed.
-  Lemma sintpy_sep {i s P Q} :
-    ⸨ P ⸩(σ s, i) ∗ ⸨ Q ⸩(σ s, i) -∗ ⸨ P ∗ Q ⸩(σ s, i).
-  Proof.
-    iIntros "[P Q]". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
-    iSplitL "P"; by iApply "to".
-  Qed.
-  Lemma sintpy_persistently {i s P} : □ ⸨ P ⸩(σ s, i) -∗ ⸨ □ P ⸩(σ s, i).
-  Proof.
-    iIntros "#P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _ !#".
-    by iApply "to".
-  Qed.
-  Lemma sintpy_bupd {i s P} : (|==> ⸨ P ⸩(σ s, i)) -∗ ⸨ |==> P ⸩(σ s, i).
-  Proof.
-    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
-    by iApply "to".
-  Qed.
-  Lemma sintpy_fupd {i s E E' P} :
-    (|={E,E'}=> ⸨ P ⸩(σ s, i)) -∗ ⸨ |={E,E'}=> P ⸩(σ s, i).
-  Proof.
-    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
-    by iApply "to".
-  Qed.
-  Lemma sintpy_later {i s P} : ▷ ⸨ P ⸩(σ s, i) -∗ ⸨ ▷{nil} P ⸩(σ s, i).
-  Proof.
-    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
-    rewrite nintp_fp_nintp. by iApply "to".
-  Qed.
-  Lemma sintpy_n_forall {i s V} {P : nPropL ([V];ᵞ )} :
-    (∀ Φ, ⸨ P /: Φ ⸩(σ s, i)) -∗ ⸨ ∀: V, P ⸩(σ s, i).
-  Proof.
-    iIntros "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _". iIntros (Ψ).
-    rewrite rew_eq_hwf. iApply "to". iApply "P".
-  Qed.
-  Lemma sintpy_n_exist {i s V} {P : nPropL ([V];ᵞ )} :
-    (∃ Φ, ⸨ P /: Φ ⸩(σ s, i)) -∗ ⸨ ∃: V, P ⸩(σ s, i).
-  Proof.
-    iDestruct 1 as (Φ) "P". iApply sintpy_byintp. iIntros (? _) "/= #to _ _".
-    iExists Φ. rewrite rew_eq_hwf. iApply "to". iApply "P".
-  Qed.
-
-  (** Introduce [○(i)] *)
-  Lemma sintpy_indir_intro {i j s P} :
-    ⸨ P ⸩(σ $∨ˢ s, i) -∗ ⸨ ○{nil}(i) P ⸩(σ s, j).
-  Proof.
-    iIntros "P". iApply sintpy_byintp. iIntros (σ' _) "/= _ #toσ' _".
-    by iApply "toσ'".
-  Qed.
-  (** Eliminate [○(i)] under a strong interpration of level [j > i] *)
-  Lemma sintpy_indir_elim {i j s P} :
-    i < j → ⸨ ○{nil}(i) P ⸩(σ s, j) -∗ ⸨ P ⸩(σ s, j).
-  Proof.
-    move=> ij. iIntros "○P". iApply sintpy_byintp.
-    iIntros (σ' _) "/= #to #toσ' #σ'to". iDestruct ("to" with "○P") as "/= ○P".
-    by iApply "σ'to".
-  Qed.
-End lemmas.
+End nsintpy.
