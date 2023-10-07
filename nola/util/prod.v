0a146f6e2055b7178608d438d56e7f13835bedea
Yusuke Matsushita <y.skm24t@gmail.com>
2023-10-08 03:12:45 +0900

    Tweak on prod'

--- nola/util/prod.v
+++ nola/util/prod.v
@@ -18,3 +18,7 @@ Notation "p .1'" := (fst' p) (at level 2, left associativity, format "p .1'")
   : nola_scope.
 Notation "p .2'" := (snd' p) (at level 2, left associativity, format "p .2'")
   : nola_scope.
+Notation "pl .*1'" := (fmap (M:=list) fst' pl)
+  (at level 2, left associativity, format "pl .*1'").
+Notation "pl .*2'" := (fmap (M:=list) snd' pl)
+  (at level 2, left associativity, format "pl .*2'").
