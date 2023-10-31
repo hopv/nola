74d170585d43d1bb9bb5b8eb36b53110e0fb92f0
Yusuke Matsushita <y.skm24t@gmail.com>
2023-11-01 04:52:25 +0900

    More on plist

--- nola/iris/plist.v
+++ nola/iris/plist.v
@@ -2,6 +2,7 @@
 
 From nola.util Require Export plist.
 From iris.bi Require Import bi.
+From iris.proofmode Require Import proofmode.
 
 Fixpoint big_sepPL {PROP : bi} {A} {F : A → Type} (Φ : ∀ a, F a → PROP) {al}
   : plist F al → PROP :=
@@ -22,4 +23,38 @@ Section big_sepPL.
     (f : ∀ a, F a → B) (Φ : B → PROP) {al} (xl : plist F al) :
     ([∗ list] y ∈ of_plist f xl, Φ y) ⊣⊢ [∗ plist] x ∈ xl, Φ (f _ x).
   Proof. elim: al xl; [done|]=>/= ?? IH [??]. by rewrite IH. Qed.
+
+  (** [big_sepPL] preserves [Persistent] *)
+  #[export] Instance big_sepPL_persistent {Φ : ∀ a, F a → PROP}
+    {al} {xl : plist F al} `{!∀ a x, Persistent (Φ a x)} :
+    Persistent ([∗ plist] x ∈ xl, Φ _ x).
+  Proof. elim: al xl; exact _. Qed.
+
+  (** Merge [big_sepPL]s *)
+  Lemma big_sepPL_sep {Φ Ψ : ∀ a, F a → PROP} {al} {xl : plist F al} :
+    ([∗ plist] x ∈ xl, Φ _ x ∗ Ψ _ x) ⊣⊢
+      ([∗ plist] x ∈ xl, Φ _ x) ∗ [∗ plist] x ∈ xl, Ψ _ x.
+  Proof.
+    elim: al xl=>/=; [move=> _; by rewrite left_id|]=> ?? IH [??].
+    rewrite IH. iSplit; iIntros "[[$$][$$]]".
+  Qed.
+  Lemma big_sepPL_sep_2 {Φ Ψ : ∀ a, F a → PROP} {al} {xl : plist F al} :
+    ([∗ plist] x ∈ xl, Φ _ x) -∗ ([∗ plist] x ∈ xl, Ψ _ x) -∗
+      [∗ plist] x ∈ xl, Φ _ x ∗ Ψ _ x.
+  Proof. rewrite big_sepPL_sep. iIntros. iFrame. Qed.
+
+  (** Modify [big_sepPL] *)
+  Lemma big_sepPL_impl {Φ Ψ : ∀ a, F a → PROP} {al} {xl : plist F al} :
+    ([∗ plist] x ∈ xl, Φ _ x) -∗ □ (∀ a x, Φ a x -∗ Ψ a x) -∗
+      [∗ plist] x ∈ xl, Ψ _ x.
+  Proof.
+    elim: al xl; [by iIntros|]=>/= ?? IH [??]. iIntros "[Φ Φl] #→".
+    iDestruct (IH with "Φl →") as "$". by iApply "→".
+  Qed.
+
+  (** [big_sepPL] and [plist_map] *)
+  Lemma big_sepPL_map {G : A → Type} (f : ∀ a, F a → G a)
+    (Φ : ∀ a, G a → PROP) {al} (xl : plist F al) :
+    ([∗ plist] x ∈ plist_map f xl, Φ _ x) ⊣⊢ [∗ plist] x ∈ xl, Φ _ (f _ x).
+  Proof. elim: al xl=>/=; [done|]=> ?? IH [??]. by rewrite IH. Qed.
 End big_sepPL.
--- nola/util/plist.v
+++ nola/util/plist.v
@@ -13,6 +13,12 @@ Fixpoint of_plist {A} {F : A → Type} {B} (f : ∀ a, F a → B) {al}
   match al with [] => λ _, [] | _ :: _ =>
     λ '(x, xl)', f _ x :: of_plist f xl end.
 
+(** [fmap] over [of_plist] *)
+Lemma of_plist_fmap {A} {F : A → Type} {B C} (f : ∀ a, F a → B) (g : B → C)
+  {al} {xl : plist F al} :
+  g <$> (of_plist f xl) = of_plist (λ a x, g (f a x)) xl.
+Proof. elim: al xl; [done|]=>/= ???[??]. by f_equiv. Qed.
+
 (** Map [plist] *)
 Fixpoint plist_map {A} {F G : A → Type} (f : ∀ a, F a → G a) {al : list A}
   : plist F al → plist G al :=
@@ -25,6 +31,16 @@ Fixpoint plist_zip {A} {F G : A → Type} {al : list A}
   match al with [] => λ _ _, () | _ :: _ =>
     λ '(x, xl)' '(y, yl)', ((x, y)', plist_zip xl yl)' end.
 
+(** [fst']/[snd'] over [plist_zip] *)
+Lemma plist_zip_fst {A} {F G : A → Type} {al}
+  {xl : plist F al} {yl : plist G al} :
+  plist_map (λ _, fst') (plist_zip xl yl) = xl.
+Proof. elim: al xl yl; [by case|]=>/= ?? IH [??][??]. by rewrite IH. Qed.
+Lemma plist_zip_snd {A} {F G : A → Type} {al}
+  {xl : plist F al} {yl : plist G al} :
+  plist_map (λ _, snd') (plist_zip xl yl) = yl.
+Proof. elim: al xl yl; [case; by case|]=>/= ?? IH [??][??]. by rewrite IH. Qed.
+
 (** [plist] over [repeat] as [vec] *)
 Fixpoint of_plist_repeat {A} {F : A → Type} {a : A} {n}
   : plist F (repeat a n) → vec (F a) n :=
@@ -33,6 +49,8 @@ Fixpoint of_plist_repeat {A} {F : A → Type} {a : A} {n}
 Fixpoint to_plist_repeat {A} {F : A → Type} {a : A} {n} (xl : vec (F a) n)
   : plist F (repeat a n) :=
   match xl with [#] => () | b ::: xl => (b, to_plist_repeat xl)' end.
+
+(** [of_plist_repeat] and [to_plist_repeat] are mutually inverse *)
 Lemma to_of_plist_repeat {A F} {a : A} {n} {xl : plist F (repeat a n)} :
   to_plist_repeat (of_plist_repeat xl) = xl.
 Proof. elim: n xl; [by case|]=>/= ? IH [??]. by rewrite IH. Qed.
@@ -49,6 +67,8 @@ Fixpoint plist_sep {A} {F : A → Type} {al bl}
   : plist F (al ++ bl) → plist F al *' plist F bl :=
   match al with [] => λ xl, ((), xl)' | _ :: _ =>
     λ '(x, xl)', let '(yl, zl)' := plist_sep xl in ((x, yl)', zl)' end.
+
+(** [plist_app] and [plist_sep] are mutually inverse *)
 Lemma plist_app_sep {A} {F : A → Type} {al bl}
   {xl : plist F al} {yl : plist F bl} :
   plist_sep (plist_app xl yl) = (xl, yl)'.
