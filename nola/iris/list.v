998398fc684aed8fbba7da187096b297b092c74e
Yusuke Matsushita <y.skm24t@gmail.com>
2023-11-11 12:53:44 +0900

    Minor tweak

--- nola/iris/list.v
+++ nola/iris/list.v
@@ -3,9 +3,10 @@
 From nola Require Export prelude.
 From iris.proofmode Require Import proofmode.
 
-(** ** On [big_sepL] *)
+(** ** On [[∗ list]] *)
 Section big_sepL.
   Context {PROP : bi} {A : Type}.
+
   #[export] Instance big_sepL_mono'' :
     Proper (pointwise_relation _ (pointwise_relation _ (flip (⊢))) ==> (=) ==>
       flip (⊢)) (big_opL (@bi_sep PROP) (A:=A)).
