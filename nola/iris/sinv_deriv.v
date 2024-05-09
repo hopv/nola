6f9f9d2d75bfea9d7cad3a578728fcad2b325873
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-05-09 22:14:54 +0900

    Tweaks on sinv_deriv

--- nola/iris/sinv_deriv.v
+++ nola/iris/sinv_deriv.v
@@ -25,15 +25,12 @@ Section sinv_deriv.
   Context `{!DerivSinv DER}.
 
   (** [sinv]: Relaxed simple invariant *)
-  Local Definition sinv_def δ P : iProp Σ :=
+  Definition sinv δ P : iProp Σ :=
     ∃ Q, □ ⸨ deriv_sinv_acsr P Q ⸩(δ) ∗ sinv_tok Q.
-  Local Lemma sinv_aux : seal sinv_def. Proof. by eexists. Qed.
-  Definition sinv := sinv_aux.(unseal).
-  Local Lemma sinv_unseal : sinv = sinv_def. Proof. exact: seal_eq. Qed.
 
   (** [sinv] is persistent *)
-  #[export] Instance sinv_persistent {δ P} : Persistent (sinv δ P).
-  Proof. rewrite sinv_unseal. exact _. Qed.
+  Fact sinv_persistent {δ P} : Persistent (sinv δ P).
+  Proof. exact _. Qed.
 
   (** World satisfaction *)
   Definition sinv_wsatd δ := sinv_wsat (deriv_sinv_intp δ).
@@ -43,8 +40,8 @@ Section sinv_deriv.
     sinv der P -∗ sinv_wsatd der -∗
       deriv_sinv_mod (⟦ P ⟧' ∗ (⟦ P ⟧' -∗ deriv_sinv_mod (sinv_wsatd der))).
   Proof.
-    rewrite sinv_unseal. iIntros "[%Q[QPQ s]] W".
-    iDestruct (der_sound with "QPQ") as "QPQ". rewrite deriv_sinv_acsr_intp.
+    iIntros "[%Q[QPQ s]] W". iDestruct (der_sound with "QPQ") as "QPQ".
+    rewrite deriv_sinv_acsr_intp.
     iDestruct (sinv_tok_acc with "s W") as "[Q cl]".
     iMod ("QPQ" with "Q") as "[$ PQ]". iIntros "!> P".
     iMod ("PQ" with "P") as "Q". iModIntro. by iApply "cl".
@@ -53,34 +50,38 @@ Section sinv_deriv.
   Context `{!Deriv (DER:=DER) ih δ}.
 
   (** Turn [sinv_tok] into [sinv] *)
-  Local Lemma sinv_tok_sinv {P} : sinv_tok P ⊢ sinv δ P.
+  Lemma sinv_tok_sinv {P} : sinv_tok P ⊢ sinv δ P.
   Proof.
-    rewrite sinv_unseal. iIntros "$ !>". iApply Deriv_intro. iIntros (? _ _).
+    iIntros "$ !>". iApply Deriv_intro. iIntros (? _ _).
     rewrite deriv_sinv_acsr_intp. iApply acsr_refl.
   Qed.
+
   (** Allocate [sinv] *)
   Lemma sinv_alloc P : sinv_wsatd δ ==∗ sinv δ P ∗ (⟦ P ⟧'(δ) -∗ sinv_wsatd δ).
   Proof. rewrite -sinv_tok_sinv. exact: sinv_tok_alloc. Qed.
 
-  (** Transform [sinv] *)
-  Lemma sinv_acsr {P Q} : □ ⸨ deriv_sinv_acsr P Q ⸩(δ) -∗ sinv δ Q -∗ sinv δ P.
+  (** Convert [sinv] with [acsr] *)
+  Lemma sinv_acsr' {P Q} : □ ⸨ deriv_sinv_acsr P Q ⸩(δ) -∗ sinv δ Q -∗ sinv δ P.
   Proof.
-    rewrite sinv_unseal. iIntros "#QPQ [%R[#RQR $]] !>".
-    iApply (Deriv_map2 with "[] QPQ RQR"). iIntros (? _ _).
-    rewrite !deriv_sinv_acsr_intp. iApply acsr_trans.
+    iIntros "#QPQ [%R[#RQR $]] !>". iApply (Deriv_map2 with "[] QPQ RQR").
+    iIntros (? _ _). rewrite !deriv_sinv_acsr_intp. iApply acsr_trans.
+  Qed.
+  Lemma sinv_acsr {P Q} :
+    □ (∀ δ, acsr deriv_sinv_mod ⟦ P ⟧'(δ) ⟦ Q ⟧'(δ)) -∗ sinv δ Q -∗ sinv δ P.
+  Proof.
+    iIntros "#PQP". iApply sinv_acsr'. iModIntro. iApply Deriv_intro.
+    iIntros (? _ _). rewrite deriv_sinv_acsr_intp. by iApply "PQP".
   Qed.
 
   (** Split [sinv] over [∗] *)
   Local Lemma sinv_sep' {PQ P Q} :
-    (∀ δ, ⟦ PQ ⟧'(δ) ≡ (⟦ P ⟧'(δ) ∗ ⟦ Q ⟧'(δ))%I) →
-    sinv δ PQ -∗ sinv δ P.
+    (∀ δ, ⟦ PQ ⟧'(δ) ≡ (⟦ P ⟧'(δ) ∗ ⟦ Q ⟧'(δ))%I) → sinv δ PQ ⊢ sinv δ P.
   Proof.
-    move=> eq. iApply sinv_acsr. iModIntro. iApply Deriv_intro.
-    iIntros (? _ _). rewrite deriv_sinv_acsr_intp. unfold acsr. rewrite !eq.
+    move=> eq. iApply sinv_acsr. iIntros "!>" (?). unfold acsr. rewrite eq.
     iApply acsr_sep_l.
   Qed.
   Lemma sinv_sep {PQ P Q} : (∀ δ, ⟦ PQ ⟧'(δ) ≡ (⟦ P ⟧'(δ) ∗ ⟦ Q ⟧'(δ))%I) →
-    sinv δ PQ -∗ sinv δ P ∗ sinv δ Q.
+    sinv δ PQ ⊢ sinv δ P ∗ sinv δ Q.
   Proof.
     iIntros (eq) "#s". iSplit; [by iApply sinv_sep'|].
     iApply (sinv_sep' with "s"). iIntros (?). by rewrite eq comm.
