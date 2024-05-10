16a199099634e1d0ff0e8d883c34880c62405b20
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-05-10 09:12:33 +0900

    Remove eq_subrel_respect

--- nola/prelude.v
+++ nola/prelude.v
@@ -8,9 +8,3 @@ From iris.prelude Require Export prelude.
 Declare Scope nola_scope.
 Delimit Scope nola_scope with nola.
 Open Scope nola_scope.
-
-(** Make [(=)] a subrelation of [==>] *)
-#[export] Instance eq_subrel_respect
-  `{subR : subrelation (A:=A) R (=)} `{subR' : subrelation (A:=B) (=) R'} :
-  subrelation (=) (R ==> R')%signature.
-Proof. move=> f _ <- a _ /subR<-. by apply subR'. Qed.
