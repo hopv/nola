f845f55f7d9d6f29695d0f0dada169bbce5833a9
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-05-10 10:31:53 +0900

    Comment tweaks on deriv

--- nola/iris/deriv.v
+++ nola/iris/deriv.v
@@ -6,7 +6,7 @@ From iris.proofmode Require Import proofmode.
 
 (** ** Preliminaries *)
 
-(** [dwrap]: Wrapper for a derivability candidate *)
+(** [dwrap]: Wrapper for a derivability *)
 
 #[projections(primitive)]
 Record dwrap (A : Type) := Dwrap { dunwrap : A }.
@@ -39,10 +39,10 @@ Proof. solve_proper. Qed.
 
 Implicit Type (JUDG : Type) (PROP : bi).
 
-(** Type for derivability *)
+(** Type for a derivability predicate *)
 Notation deriv_ty JUDG PROP := (dwrap (JUDG -d> PROP)).
 
-(** [derivs]: Derivation structure *)
+(** [derivst]: Derivation structure *)
 Structure derivst (PROP : bi) : Type := Derivst {
   derivst_judg :> Type;
   (** Interpretation parameterized over derivability candidates *)
@@ -60,14 +60,16 @@ Module DerivIntpNotation.
 End DerivIntpNotation.
 Import DerivIntpNotation.
 
-(** Soundness of a candidate [δ] with respect the semantics by [δ'] *)
+(** Soundness of a derivability [δ] with respect to the semantics at [δ'] *)
 Definition dsound {PROP} {DER : derivst PROP} (δ δ' : deriv_ty DER PROP) : PROP
   := ∀ J, ⸨ J ⸩(δ) -∗ ⟦ J ⟧(δ').
 
 Definition dtrans {JUDG PROP} (δ δ' : deriv_ty JUDG PROP) : PROP :=
   ∀ J, ⸨ J ⸩(δ) -∗ ⸨ J ⸩(δ').
 
-(** ** [Deriv] : Derivability candidate *)
+(** ** [Deriv ih δ] : [δ] is a good derivability predicate
+
+  [ih] is the inductive hypothesis, used for parameterized induction *)
 
 Inductive Deriv {PROP} {DER : derivst PROP}
   (ih : deriv_ty DER PROP → Prop) (δ : deriv_ty DER PROP) : Prop := {
@@ -80,15 +82,15 @@ Inductive Deriv {PROP} {DER : derivst PROP}
 }.
 Existing Class Deriv.
 
-(** Get the candidate [⸨ J ⸩(δ)] by the interpretaion *)
+(** Get the derivability [⸨ J ⸩(δ)] by the interpretaion *)
 Lemma Deriv_byintp `{!@Deriv PROP DER ih δ} {J} :
-  (∀ δ', (* Take any candidate [δ'] *) ⌜Deriv ih δ'⌝ →
-    (* Get access to the inductive hypothesis *) ⌜ih δ'⌝ →
-    (* Turn the base candidate into the sematics by the given candidate *)
+  ((* Take any good derivability predicate [δ'] *) ∀ δ', ⌜Deriv ih δ'⌝ →
+    (* Can use the inductive hypothesis *) ⌜ih δ'⌝ →
+    (* Can turn [δ] into the semantics at [δ'] *)
       □ dsound δ δ' -∗
-    (* Turn the base candidate into the given candidate *) □ dtrans δ δ' -∗
-    (* The semantics by the given candidate *) ⟦ J ⟧(δ'))
-  -∗ (* The base candidate *) ⸨ J ⸩(δ).
+    (* Can turn [δ] into [δ'] *) □ dtrans δ δ' -∗
+    (* The semantics at [δ'] *) ⟦ J ⟧(δ'))
+  -∗ (* The derivability at [δ] *) ⸨ J ⸩(δ).
 Proof.
   have X := (@Deriv_byintp' _ _ ih δ). move: X=> [dy[dyto byintp]]. iIntros "→".
   iApply byintp. iIntros (δ' dyd'). apply dyto in dyd'. by iApply "→".
@@ -114,14 +116,14 @@ Proof.
   iApply "big"; iPureIntro; [done|]. split; by [apply to|].
 Qed.
 
-(** Introduce a candidate *)
+(** Introduce a derivability via semantics *)
 Lemma Deriv_intro `{!@Deriv PROP DER ih δ} {J} :
   (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⟦ J ⟧(δ')) ⊢ ⸨ J ⸩(δ).
 Proof.
   iIntros "∀". iApply Deriv_byintp. iIntros (???) "_ _". by iApply "∀".
 Qed.
 
-(** Update candidates *)
+(** Map derivabilities via semantics *)
 Lemma Deriv_map `{!@Deriv PROP DER ih δ} {J J'} :
   (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⟦ J ⟧(δ') -∗ ⟦ J' ⟧(δ')) -∗
   ⸨ J ⸩(δ) -∗ ⸨ J' ⸩(δ).
@@ -155,7 +157,7 @@ Proof.
   iDestruct "Js" as "[J Js]". iSplitL "J"; by [iApply "→"|iApply "IH"].
 Qed.
 
-(** ** [der]: Derivability *)
+(** ** [der]: The best derivability predicate *)
 
 (** [der_gen]: What becomes [der] by taking [bi_least_fixpoint] *)
 Definition der_gen {PROP} {DER : derivst PROP} (self : DER → PROP)
@@ -171,7 +173,7 @@ Proof.
     by iApply "ΦΨ".
 Qed.
 
-(** [der]: Derivability *)
+(** [der]: The best derivability predicate *)
 Definition der_def {PROP} {DER : derivst PROP} : deriv_ty DER PROP :=
   Dwrap (bi_least_fixpoint (A:=leibnizO _) (@der_gen PROP DER)).
 Lemma der_aux : seal (@der_def). Proof. by eexists. Qed.
