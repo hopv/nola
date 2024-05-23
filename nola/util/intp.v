4ddddcebceab462285570bc9b3d0a51842ed16f0
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-05-23 14:08:11 +0900

    Introduce Intp

--- nola/util/intp.v
+++ nola/util/intp.v
@@ -2,7 +2,17 @@
 
 From nola Require Export prelude.
 
-(** Parameterized interpretation *)
+(** ** [Intp]: Interpretation *)
+Class Intp (A B : Type) := intp : A → B.
+Hint Mode Intp ! - : typeclass_instances.
+Module IntpNotation.
+  Notation "⟦ ⟧" := (intp) (format "⟦ ⟧").
+  Notation "⟦ ⟧@{ A }" := (intp (A:=A)) (only parsing).
+  Notation "⟦ a ⟧" := (intp a) (format "⟦  '[' a  ']' ⟧").
+  Notation "⟦ a ⟧@{ A }" := (intp (A:=A) a) (only parsing).
+End IntpNotation.
+
+(** ** [Pintp]: Parameterized interpretation *)
 Class Pintp (X A B : Type) := pintp : X → A → B.
 Hint Mode Pintp - ! - : typeclass_instances.
 Module PintpNotation.
