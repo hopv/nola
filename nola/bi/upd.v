c4fb58462dc1e00605b04475a782782068996ff5
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-05-23 17:57:41 +0900

    Context tweak on acsr

--- nola/bi/upd.v
+++ nola/bi/upd.v
@@ -634,14 +634,16 @@ Section acsr.
   (** ** [acsr]: Accessor getting [P] from [Q] via the modality [M] *)
   Definition acsr M P Q : PROP := Q -∗ M (P ∗ (P -∗ M Q))%I.
 
+  Context `{!GenUpd (PROP:=PROP) M}.
+
   (** [acsr] is proper *)
-  #[export] Instance acsr_ne `{!GenUpd M} : NonExpansive2 (acsr M).
+  #[export] Instance acsr_ne : NonExpansive2 (acsr M).
   Proof. solve_proper. Qed.
 
   (** [acsr] is reflexive and transitive *)
-  Lemma acsr_refl `{!GenUpd M} {P} : ⊢ acsr M P P.
+  Lemma acsr_refl {P} : ⊢ acsr M P P.
   Proof. by iIntros "$ !> $". Qed.
-  Lemma acsr_trans `{!GenUpd M} {P Q R} :
+  Lemma acsr_trans {P Q R} :
     acsr M P Q -∗ acsr M Q R -∗ acsr M P R.
   Proof.
     iIntros "QPQ RQR R". iMod ("RQR" with "R") as "[Q QR]".
@@ -650,8 +652,8 @@ Section acsr.
   Qed.
 
   (** [acsr] over [∗] *)
-  Lemma acsr_sep_l `{!GenUpd M} {P Q} : ⊢ acsr M P (P ∗ Q).
+  Lemma acsr_sep_l {P Q} : ⊢ acsr M P (P ∗ Q).
   Proof. by iIntros "[$$] !> $". Qed.
-  Lemma acsr_sep_r `{!GenUpd M} {P Q} : ⊢ acsr M Q (P ∗ Q).
+  Lemma acsr_sep_r {P Q} : ⊢ acsr M Q (P ∗ Q).
   Proof. by iIntros "[$$] !> $". Qed.
 End acsr.
