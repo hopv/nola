20b1c5c62031c289a2c56a5853fbf88a4a39872f
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-09-26 16:19:30 +0900

    Minor variable name tweak

--- nola/util/tagged.v
+++ nola/util/tagged.v
@@ -5,24 +5,24 @@ From iris.algebra Require Import ofe.
 
 (** ** [tagged]: Type tagged with a ghost id *)
 #[projections(primitive)]
-Record tagged (Id : Type) (A : Type) := Tagged { untag : A; }.
+Record tagged (ID : Type) (A : Type) := Tagged { untag : A; }.
 Add Printing Constructor tagged.
 Arguments Tagged {_ _}. Arguments untag {_ _}.
 
 (** OFE for [tagged] *)
-Definition tagged_equiv {Id} {A : ofe} : relation (tagged Id A) :=
+Definition tagged_equiv {ID} {A : ofe} : relation (tagged ID A) :=
   λ '(Tagged a) '(Tagged a'), a ≡ a'.
-Definition tagged_dist {Id} {A : ofe} n : relation (tagged Id A) :=
+Definition tagged_dist {ID} {A : ofe} n : relation (tagged ID A) :=
   λ '(Tagged a) '(Tagged a'), a ≡{n}≡ a'.
-Program Canonical taggedO (Id : Type) (A : ofe) :=
-  @Ofe (tagged Id A) tagged_equiv tagged_dist _.
+Program Canonical taggedO (ID : Type) (A : ofe) :=
+  @Ofe (tagged ID A) tagged_equiv tagged_dist _.
 Next Obligation.
   move=> ??. split=> >; [by apply equiv_dist| |by apply dist_lt].
   unfold dist, tagged_dist. split; [by move..|]=> ???. apply transitivity.
 Qed.
 (** [Tagged] is non-expansive *)
-#[export] Instance tagged_ne {Id} {A : ofe} : NonExpansive (@Tagged Id A).
+#[export] Instance tagged_ne {ID} {A : ofe} : NonExpansive (@Tagged ID A).
 Proof. solve_proper. Qed.
-#[export] Instance tagged_proper {Id} {A : ofe} :
-  Proper ((≡) ==> (≡)) (@Tagged Id A).
+#[export] Instance tagged_proper {ID} {A : ofe} :
+  Proper ((≡) ==> (≡)) (@Tagged ID A).
 Proof. solve_proper. Qed.
