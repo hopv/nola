46f22a26090a33a79ccf8b324ccb6eba36f5b653
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-11-03 00:11:37 +0900

    Reformulate store using dinv

--- nola/iris/store.v
+++ nola/iris/store.v
@@ -1,155 +1,46 @@
 (** * Stored propositions *)
 
 From nola.bi Require Export internal modw.
-From nola.iris Require Export iprop.
-From nola.iris Require Import sinv.
-From iris.base_logic Require Import lib.token.
+From nola.iris Require Export cif dinv.
+From iris.base_logic Require Export lib.token.
 From iris.proofmode Require Import proofmode.
-Import iPropAppNotation ModwNotation.
+Import iPropAppNotation ModwNotation DsemNotation.
 
-Implicit Type (FML : oFunctor) (i : positive).
-
-(** Formula for stored propositions *)
-Local Definition store_fml FML : oFunctor := boolO * FML.
-
-(** Formula for stored propositions *)
-Class storeGpreS FML Σ := StoreGpreS {
-  storeGpreS_sinv : sinvGpreS (store_fml FML) Σ;
-  storeGpreS_token : tokenG Σ;
-}.
-Local Existing Instance storeGpreS_sinv.
-Local Existing Instance storeGpreS_token.
-Class storeGS FML Σ := StoreGS {
-  storeGS_sinv : sinvGS (store_fml FML) Σ;
-  storeGS_token : tokenG Σ;
-}.
-Local Existing Instance storeGS_sinv. Local Existing Instance storeGS_token.
-Definition storeΣ FML `{!oFunctorContractive FML} :=
-  #[sinvΣ (store_fml FML); tokenΣ].
-#[export] Instance subG_storeΣ
-  `{!oFunctorContractive FML, !subG (storeΣ FML) Σ} : storeGpreS FML Σ.
-Proof. solve_inG. Qed.
+Implicit Type FML : oFunctor.
 
 Section store.
-  Context `{!storeGS FML Σ}.
-  Implicit Type (Px : FML $oi Σ) (bPx : store_fml FML $oi Σ).
+  Context `{!dinvGS (cifOF CON) Σ, tokenG Σ, !Csem CON JUDG Σ}.
+  Implicit Type Px : cif CON Σ.
 
   (** [store_tok]: Exclusive stored proposition token *)
-  Local Definition store_tok_def (Px : FML $oi Σ) : iProp Σ :=
-    ∃ i, token i ∗ sinv_tok i (true, Px).
+  Local Definition store_tok_def Px : iProp Σ :=
+    ∃ γ, token γ ∗ dinv_tok (Px ∨ ▷ token γ)%cif.
   Local Definition store_tok_aux : seal store_tok_def. Proof. by eexists. Qed.
   Definition store_tok := store_tok_aux.(unseal).
   Local Lemma store_tok_unseal : store_tok = store_tok_def.
   Proof. exact: seal_eq. Qed.
 
-  (** [pstore_tok]: Persistent stored proposition token *)
-  Local Definition pstore_tok_def (Px : FML $oi Σ) : iProp Σ :=
-    ∃ i, sinv_tok i (false, Px).
-  Local Definition pstore_tok_aux : seal pstore_tok_def. Proof. by eexists. Qed.
-  Definition pstore_tok := pstore_tok_aux.(unseal).
-  Local Lemma pstore_tok_unseal : pstore_tok = pstore_tok_def.
-  Proof. exact: seal_eq. Qed.
-
-  (** [store_tok] and [pstore_tok] are non-expansive *)
+  (** [store_tok] is non-expansive *)
   #[export] Instance store_tok_ne : NonExpansive store_tok.
   Proof. rewrite store_tok_unseal. solve_proper. Qed.
   #[export] Instance store_tok_proper : Proper ((≡) ==> (⊣⊢)) store_tok.
   Proof. apply ne_proper, _. Qed.
-  #[export] Instance pstore_tok_ne : NonExpansive pstore_tok.
-  Proof. rewrite pstore_tok_unseal. solve_proper. Qed.
-  #[export] Instance pstore_tok_proper : Proper ((≡) ==> (⊣⊢)) pstore_tok.
-  Proof. apply ne_proper, _. Qed.
-  (** [pstore_tok] is persistent *)
-  #[export] Instance pstore_tok_persistent {Px} : Persistent (pstore_tok Px).
-  Proof. rewrite pstore_tok_unseal. exact _. Qed.
-  (** [store_tok] and [pstore_tok] are timeless for discrete formulas *)
-  #[export] Instance store_tok_timeless `{!Discrete Px} :
-    Timeless (store_tok Px).
-  Proof. rewrite store_tok_unseal. exact _. Qed.
-  #[export] Instance pstore_tok_timeless `{!Discrete Px} :
-    Timeless (pstore_tok Px).
-  Proof. rewrite pstore_tok_unseal. exact _. Qed.
-
-  (** Semantics *)
-  Local Definition store_sem (sm : FML $oi Σ → iProp Σ) i bPx : iProp Σ :=
-    (if bPx.1 then sm bPx.2 ∨ token i else □ sm bPx.2)%I.
-  Arguments store_sem _ _ _ /.
-  (** [store_sem sm] is non-expansive when [sm] is *)
-  Local Lemma store_sem_ne {sm} :
-    internal_ne sm ⊢@{iProp Σ} ∀ i, internal_ne (store_sem sm i).
-  Proof.
-    iIntros "#Ne" (?[b ?][??]). rewrite prod_equivI /= discrete_eq.
-    rewrite leibniz_equiv_iff. iIntros "[%eq ≡]". move: eq=> <-.
-    case: b; by iRewrite ("Ne" with "≡").
-  Qed.
-
-  (** World satisfaction *)
-  Local Definition store_wsat_def (sm : FML $oi Σ -d> iProp Σ) : iProp Σ :=
-    sinv_wsat (store_sem sm).
-  Local Definition store_wsat_aux : seal store_wsat_def. Proof. by eexists. Qed.
-  Definition store_wsat := store_wsat_aux.(unseal).
-  Local Lemma store_wsat_unseal : store_wsat = store_wsat_def.
-  Proof. exact: seal_eq. Qed.
-
-  (** [store_wsat] is non-expansive *)
-  #[export] Instance store_wsat_ne : NonExpansive store_wsat.
-  Proof.
-    rewrite store_wsat_unseal. unfold store_wsat_def, store_sem. solve_proper.
-  Qed.
-  #[export] Instance store_wsat_proper : Proper ((≡) ==> (≡)) store_wsat.
-  Proof. apply ne_proper, _. Qed.
 
   (** Allocate [store_tok] *)
-  Lemma store_tok_alloc {sm} Px : sm Px =[store_wsat sm]=∗ store_tok Px.
+  Lemma store_tok_alloc {δ} Px : ⟦ Px ⟧(δ) =[dinv_wsat ⟦⟧(δ)]=∗ store_tok Px.
   Proof.
-    rewrite store_tok_unseal store_wsat_unseal. iIntros "Px W".
-    iDestruct (sinv_tok_alloc_suspend (FML:=store_fml _) (true, Px) with "W")
-      as (I) "big".
-    iMod (token_alloc_strong (.∉ I)) as (??) "$".
-    { apply (pred_infinite_set (C:=gset positive))=> X. exists (fresh (I ∪ X)).
-      apply not_elem_of_union, is_fresh. }
-    iMod ("big" with "[//]") as "[$ →W]". iModIntro. iApply "→W". by iLeft.
-  Qed.
-
-  (** Allocate [pstore_tok] suspending the world satisfaction *)
-  Lemma pstore_tok_alloc_suspend {sm} Px :
-    store_wsat sm ==∗ pstore_tok Px ∗ (□ sm Px -∗ store_wsat sm).
-  Proof.
-    rewrite pstore_tok_unseal store_wsat_unseal. iIntros "W".
-    iDestruct (sinv_tok_alloc_suspend (FML:=store_fml _) (false, Px) with "W")
-      as (I) "big".
-    iMod ("big" $! (fresh I) with "[%]") as "[#inv →W]". { apply is_fresh. }
-    iModIntro. iFrame "inv". iIntros "?". by iApply "→W".
-  Qed.
-  (** Allocate [pstore_tok] *)
-  Lemma pstore_tok_alloc {sm} Px : □ sm Px =[store_wsat sm]=∗ pstore_tok Px.
-  Proof.
-    iIntros "? W". iMod (pstore_tok_alloc_suspend with "W") as "[$ →W]".
-    iModIntro. by iApply "→W".
+    rewrite store_tok_unseal. iIntros "Px W". iMod token_alloc as (γ) "$".
+    iMod (dinv_tok_alloc_suspend (FML:=cifOF _) (Px ∨ ▷ token γ)%cif
+      with "W") as "/=[$ cl]".
+    iModIntro. iApply "cl". iFrame.
   Qed.
 
   (** Access the content of [store_tok] *)
-  Lemma store_tok_acc {sm} Px : store_tok Px -∗[store_wsat sm] sm Px.
+  Lemma store_tok_acc {δ} Px : store_tok Px -∗[dinv_wsat ⟦⟧(δ)]◇ ⟦ Px ⟧(δ).
   Proof.
-    rewrite store_tok_unseal store_wsat_unseal. iIntros "[%[tok inv]] W".
-    iDestruct (sinv_tok_acc with "inv W") as "/=[[$|tok'] →W]"; last first.
-    { iDestruct (token_exclusive with "tok tok'") as %[]. }
-    iApply "→W". by iRight.
-  Qed.
-  (** Access the content of [pstore_tok] *)
-  Lemma pstore_tok_acc {sm} Px : pstore_tok Px -∗[store_wsat sm] □ sm Px.
-  Proof.
-    rewrite pstore_tok_unseal store_wsat_unseal. iIntros "[% inv] W".
-    iDestruct (sinv_tok_acc with "inv W") as "/=[#Px →W]". iFrame "Px".
-    by iApply "→W".
+    rewrite store_tok_unseal. iIntros "[%[t i]] W".
+    iDestruct (dinv_tok_acc with "i W") as "/=[[$|>t'] →W]"; last first.
+    { iDestruct (token_exclusive with "t t'") as %[]. }
+    iModIntro. iApply "→W". by iRight.
   Qed.
 End store.
-
-(** Allocate [store_wsat] *)
-Lemma store_wsat_alloc `{!storeGpreS FML Σ} :
-  ⊢ |==> ∃ _ : storeGS FML Σ, ∀ sm, internal_ne sm -∗ store_wsat sm.
-Proof.
-  iMod sinv_wsat_alloc as (γ) "W". iModIntro. iExists (StoreGS _ _ _ _).
-  iIntros (?) "Ne". rewrite store_wsat_unseal. iApply "W".
-  by iApply store_sem_ne.
-Qed.
--- nola/iris/store_deriv.v
+++ nola/iris/store_deriv.v
@@ -6,14 +6,14 @@ From nola.iris Require Export store.
 From iris.proofmode Require Import proofmode.
 Import iPropAppNotation ModwNotation DsemNotation.
 
-Implicit Type (Σ : gFunctors) (FM : ofe) (b : bool).
+Implicit Type (Σ : gFunctors) (FM : ofe).
 
 (** ** Judgment *)
 
 (* [storeJT]: Judgment type for [store] *)
 Variant storeJT_id FM := .
-Definition storeJT (FM : ofe) : ofe :=
-  (** Accessor judgment *) tagged (storeJT_id FM) (bool * FM).
+Definition storeJT FM : ofe :=
+  (** Accessor judgment *) tagged (storeJT_id FM) FM.
 (** [storeJ]: [storeJT] registered *)
 Notation storeJ FM JUDG := (inJ (storeJT FM) JUDG).
 
@@ -22,56 +22,37 @@ Section storeJ.
   Implicit Type (δ : JUDG -n> iProp Σ) (Px : FM).
 
   (** Accessor judgment *)
-  Local Definition store_jacsr b Px : JUDG := store_j (Tagged (b, Px)).
-  Local Instance store_jacsr_ne {N} : NonExpansive (store_jacsr N).
-  Proof. unfold store_jacsr=> ????. f_equiv. by split. Qed.
+  Local Definition store_jacsr Px : JUDG := store_j (Tagged Px).
+  Local Instance store_jacsr_ne : NonExpansive store_jacsr.
+  Proof. solve_proper. Qed.
 
-  (** [store]: Relaxed exclsuive stored proposition *)
-  Local Definition store_def δ (Px : FM) : iProp Σ := δ (store_jacsr true Px).
+  (** [store]: Relaxed stored proposition *)
+  Local Definition store_def δ (Px : FM) : iProp Σ := δ (store_jacsr Px).
   Local Lemma store_aux : seal store_def. Proof. by eexists. Qed.
   Definition store := store_aux.(unseal).
   Local Lemma store_unseal : store = store_def. Proof. exact: seal_eq. Qed.
 
-  (** [pstore]: Relaxed persistent stored proposition *)
-  Local Definition pstore_def δ (Px : FM) : iProp Σ :=
-    □ δ (store_jacsr false Px).
-  Local Lemma pstore_aux : seal pstore_def. Proof. by eexists. Qed.
-  Definition pstore := pstore_aux.(unseal).
-  Local Lemma pstore_unseal : pstore = pstore_def. Proof. exact: seal_eq. Qed.
-
-  (** [pstore] is persistent *)
-  #[export] Instance pstore_persistent {δ Px} : Persistent (pstore δ Px).
-  Proof. rewrite pstore_unseal. exact _. Qed.
-
-  (** [store] and [pstore] are non-expansive *)
+  (** [store] is non-expansive *)
   #[export] Instance store_ne {δ} : NonExpansive (store δ).
   Proof. rewrite store_unseal. solve_proper. Qed.
   #[export] Instance store_proper {δ} : Proper ((≡) ==> (⊣⊢)) (store δ).
-  Proof. apply ne_proper, _. Qed.
-  #[export] Instance pstore_ne {δ} : NonExpansive (pstore δ).
-  Proof. rewrite pstore_unseal. solve_proper. Qed.
-  #[export] Instance pstore_proper {δ} : Proper ((≡) ==> (⊣⊢)) (pstore δ).
   Proof. apply ne_proper, _. Qed.
 End storeJ.
 
 (** Notation *)
 Notation stored := (store der).
-Notation pstored := (pstore der).
 
 Section storeJ.
-  Context `{!storeGS FML Σ}.
-  Implicit Type (P : iProp Σ) (Px : FML $oi Σ).
-
-  Context `{!storeJ (FML $oi Σ) JUDG, !Dsem JUDG (FML $oi Σ) (iProp Σ),
-    !Jsem JUDG (iProp Σ)}.
-  Implicit Type δ : JUDG -n> iProp Σ.
+  Context `{!dinvGS FML Σ, !storeJ (FML $oi Σ) JUDG,
+    !Dsem JUDG (FML $oi Σ) (iProp Σ), !Jsem JUDG (iProp Σ)}.
+  Implicit Type (δ : JUDG -n> iProp Σ).
 
   (** [storeJT_sem]: Semantics of [storeJT] *)
-  Definition storeJT_sem δ (bPx : storeJT (FML $oi Σ)) : iProp Σ :=
-    |->[store_wsat ⟦⟧(δ)] (□?(negb bPx.(untag).1) ⟦ bPx.(untag).2 ⟧(δ)).
+  Definition storeJT_sem δ (Px : storeJT (FML $oi Σ))  : iProp Σ :=
+    |->[dinv_wsat ⟦⟧(δ)]◇ (⟦ Px.(untag) ⟧(δ)).
   (** [storeJT_sem] is non-expansive *)
   #[export] Instance storeJT_sem_ne {δ} : NonExpansive (storeJT_sem δ).
-  Proof. move=> ?[[??]][[??]][/=/leibniz_equiv_iff<-?]. solve_proper. Qed.
+  Proof. solve_proper. Qed.
   (** [Dsem] over [storeJT] *)
   #[export] Instance storeJT_dsem : Dsem JUDG (storeJT (FML $oi Σ)) (iProp Σ) :=
     DSEM storeJT_sem _.
@@ -81,43 +62,27 @@ Arguments storeJT_sem {_ _ _ _} _ _ /.
 Notation storeJS FML JUDG Σ := (inJS (storeJT (FML $oi Σ)) JUDG (iProp Σ)).
 
 Section store_deriv.
-  Context `{!storeGS FML Σ, !storeJ (FML $oi Σ) JUDG,
-    !Dsem JUDG (FML $oi Σ) (iProp Σ), !Jsem JUDG (iProp Σ),
-    !storeJS FML JUDG Σ}.
-  Implicit Type Px Qx Rx : FML $oi Σ.
+  Context `{!dinvGS (cifOF CON) Σ, !storeJ (cifO CON Σ) JUDG,
+    !Csem CON JUDG Σ, !Jsem JUDG (iProp Σ), !storeJS (cifOF CON) JUDG Σ}.
+  Implicit Type Px Qx Rx : cif CON Σ.
 
   (** Access using [stored] *)
-  Lemma stored_acc {Px} : stored Px -∗[store_wsat ⟦⟧] ⟦ Px ⟧.
+  Lemma stored_acc {Px} : stored Px -∗[dinv_wsat ⟦⟧]◇ ⟦ Px ⟧.
   Proof.
     rewrite store_unseal. iIntros "accPx".
     iDestruct (der_sound with "accPx") as "accPx". by rewrite in_js.
   Qed.
-  (** Access using [pstored] *)
-  Lemma pstored_acc {Px} : pstored Px -∗[store_wsat ⟦⟧] □ ⟦ Px ⟧.
-  Proof.
-    rewrite pstore_unseal. iIntros "accPx".
-    iDestruct (der_sound with "accPx") as "accPx". by rewrite in_js.
-  Qed.
 
-  Context `{!Deriv (JUDG:=JUDG) ih δ}.
+  Context `{!Deriv (JUDG:=JUDG) ih δ, !tokenG Σ}.
 
   (** Turn an accessor into [store] *)
   Lemma store_acsr_store {Px} :
     (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⌜dinto δ δ'⌝ →
-      |->[store_wsat ⟦⟧(δ')] ⟦ Px ⟧(δ')) ⊢
+      |->[dinv_wsat ⟦⟧(δ')]◇ ⟦ Px ⟧(δ')) ⊢
       store δ Px.
   Proof.
     rewrite store_unseal. iIntros "big". iApply Deriv_factor. iIntros (????).
     rewrite in_js /=. by iApply "big".
-  Qed.
-  (** Turn [store_acsr] into [pstore] *)
-  Lemma store_acsr_pstore {Px} :
-    □ (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⌜dinto δ δ'⌝ →
-      |->[store_wsat ⟦⟧(δ')] □ ⟦ Px ⟧(δ')) ⊢
-      pstore δ Px.
-  Proof.
-    rewrite pstore_unseal. iIntros "#big !>". iApply Deriv_factor.
-    iIntros (????). rewrite in_js /=. by iApply "big".
   Qed.
 
   (** Turn [store_tok] into [store] *)
@@ -125,40 +90,31 @@ Section store_deriv.
   Proof.
     rewrite -store_acsr_store. iIntros "?" (????). by iApply store_tok_acc.
   Qed.
-  (** Turn [pstore_tok] into [pstore] *)
-  Lemma pstore_tok_pstore {Px} : pstore_tok Px ⊢ pstore δ Px.
+  (** Turn [dinv_tok] over a persistent proposition into [□ store] *)
+  Lemma dinv_tok_persistent_store {Px} : (∀ δ', Persistent ⟦ Px ⟧(δ')) →
+    dinv_tok Px ⊢ □ store δ Px.
   Proof.
-    rewrite -store_acsr_pstore. iIntros "#?" (????) "!>".
-    by iApply pstore_tok_acc.
+    rewrite -store_acsr_store=> ?. iIntros "#i !>" (????).
+    by iMod (dinv_tok_acc_persistent with "i") as "$".
   Qed.
 
   (** Allocate [store] *)
-  Lemma store_alloc Px : ⟦ Px ⟧(δ) =[store_wsat ⟦⟧(δ)]=∗ store δ Px.
+  Lemma store_alloc Px : ⟦ Px ⟧(δ) =[dinv_wsat ⟦⟧(δ)]=∗ store δ Px.
   Proof. rewrite -store_tok_store. exact: store_tok_alloc. Qed.
-  (** Allocate [pstore] suspending the world satisfaction *)
-  Lemma pstore_alloc_suspend Px :
-    store_wsat ⟦⟧(δ) ==∗ pstore δ Px ∗ (□ ⟦ Px ⟧(δ) -∗ store_wsat ⟦⟧(δ)).
-  Proof. rewrite -pstore_tok_pstore. exact: pstore_tok_alloc_suspend. Qed.
-  Lemma pstore_alloc Px : □ ⟦ Px ⟧(δ) =[store_wsat ⟦⟧(δ)]=∗ pstore δ Px.
-  Proof. rewrite -pstore_tok_pstore. exact: pstore_tok_alloc. Qed.
+  (** Allocate [□ store] *)
+  Lemma pstore_alloc Px : (∀ δ', Persistent ⟦ Px ⟧(δ')) →
+    ⟦ Px ⟧(δ) =[dinv_wsat ⟦⟧(δ)]=∗ □ store δ Px.
+  Proof.
+    move=> ?. rewrite -dinv_tok_persistent_store. exact: dinv_tok_alloc.
+  Qed.
 
   (** Convert [store] with [-∗] *)
   Lemma store_wand {Px Qx} :
     (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⌜dinto δ δ'⌝ →
-      ⟦ Px ⟧(δ') -∗[store_wsat ⟦⟧(δ')] ⟦ Qx ⟧(δ')) -∗
+      ⟦ Px ⟧(δ') -∗[dinv_wsat ⟦⟧(δ')]◇ ⟦ Qx ⟧(δ')) -∗
       store δ Px -∗ store δ Qx.
   Proof.
     rewrite store_unseal. iIntros "PQ accPx". iApply Deriv_factor.
-    iIntros (??? to). rewrite /store_def to !in_js /=. iMod "accPx" as "Px".
-    by iApply "PQ".
-  Qed.
-  (** Convert [pstore] with [-∗] *)
-  Lemma pstore_wand {Px Qx} :
-    □ (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⌜dinto δ δ'⌝ →
-      □ ⟦ Px ⟧(δ') -∗[store_wsat ⟦⟧(δ')] □ ⟦ Qx ⟧(δ')) -∗
-      pstore δ Px -∗ pstore δ Qx.
-  Proof.
-    rewrite pstore_unseal. iIntros "#PQ #accPx !>". iApply Deriv_factor.
     iIntros (??? to). rewrite /store_def to !in_js /=. iMod "accPx" as "Px".
     by iApply "PQ".
   Qed.
@@ -166,21 +122,10 @@ Section store_deriv.
   (** Merge [store]s *)
   Lemma store_merge {Px Qx Rx} :
     (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⌜dinto δ δ'⌝ →
-      ⟦ Px ⟧(δ') -∗ ⟦ Qx ⟧(δ') -∗[store_wsat ⟦⟧(δ')] ⟦ Rx ⟧(δ')) -∗
+      ⟦ Px ⟧(δ') -∗ ⟦ Qx ⟧(δ') -∗[dinv_wsat ⟦⟧(δ')]◇ ⟦ Rx ⟧(δ')) -∗
       store δ Px -∗ store δ Qx -∗ store δ Rx.
   Proof.
     rewrite store_unseal. iIntros "PQR accPx accQx". iApply Deriv_factor.
-    iIntros (??? to). rewrite /store_def !to !in_js /=.
-    iMod "accPx" as "Px". iMod "accQx" as "Qx".
-    iApply ("PQR" with "[//] [//] [//] Px Qx").
-  Qed.
-  (** Merge [pstore]s *)
-  Lemma pstore_merge {Px Qx Rx} :
-    □ (∀ δ', ⌜Deriv ih δ'⌝ → ⌜ih δ'⌝ → ⌜dinto δ δ'⌝ →
-      □ ⟦ Px ⟧(δ') -∗ □ ⟦ Qx ⟧(δ') -∗[store_wsat ⟦⟧(δ')] □ ⟦ Rx ⟧(δ')) -∗
-      pstore δ Px -∗ pstore δ Qx -∗ pstore δ Rx.
-  Proof.
-    rewrite pstore_unseal. iIntros "#PQR #accPx #accQx !>". iApply Deriv_factor.
     iIntros (??? to). rewrite /store_def !to !in_js /=.
     iMod "accPx" as "Px". iMod "accQx" as "Qx".
     iApply ("PQR" with "[//] [//] [//] Px Qx").
@@ -189,65 +134,45 @@ End store_deriv.
 
 (** ** Constructor *)
 
-From nola.iris Require Import cif.
-
 (** [storeCT]: Constructor for [store] *)
 Variant storeCT_id := .
 Definition storeCT :=
-  Cifcon storeCT_id bool (λ _, Empty_set) (λ _, unit) (λ _, unitO) _.
+  Cifcon storeCT_id unit (λ _, Empty_set) (λ _, unit) (λ _, unitO) _.
 (** [storeC]: [storeCT] registered *)
 Notation storeC := (inC storeCT).
 
 Section storeC.
   Context `{!storeC CON} {Σ}.
   Implicit Type Px : cif CON Σ.
-  (** Formulas *)
+  (** Formula for [store] *)
   Definition cif_store Px : cif CON Σ :=
-    cif_in storeCT true nullary (unary Px) ().
-  Definition cif_pstore Px : cif CON Σ :=
-    cif_in storeCT false nullary (unary Px) ().
-  (** The formulas are non-expansive *)
+    cif_in storeCT () nullary (unary Px) ().
+  (** [cif_store] is non-expansive *)
   #[export] Instance cif_store_ne : NonExpansive cif_store.
   Proof. move=> ????. apply cif_in_ne; solve_proper. Qed.
   #[export] Instance cif_store_proper : Proper ((≡) ==> (≡)) cif_store.
   Proof. apply ne_proper, _. Qed.
-  #[export] Instance cif_pstore_ne : NonExpansive cif_pstore.
-  Proof. move=> ????. apply cif_in_ne; solve_proper. Qed.
-  #[export] Instance cif_pstore_proper : Proper ((≡) ==> (≡)) cif_pstore.
-  Proof. apply ne_proper, _. Qed.
-  (** The formulas are productive *)
+  (** [cif_store] is productive *)
   #[export] Instance cif_store_productive : Productive cif_store.
-  Proof.
-    move=> ????. apply cif_in_preserv_productive=>//. by apply fun_proeq_later.
-  Qed.
-  #[export] Instance cif_pstore_productive : Productive cif_pstore.
   Proof.
     move=> ????. apply cif_in_preserv_productive=>//. by apply fun_proeq_later.
   Qed.
 
   Context `{!storeGS (cifOF CON) Σ, !storeJ (cifO CON Σ) JUDG}.
-  (** Semantics of [storeCT] *)
-  Definition storeCT_sem δ b : cif CON Σ → iProp Σ :=
-    if b then store δ else pstore δ.
   #[export] Program Instance storeCT_ecsem : Ecsem storeCT CON JUDG Σ :=
-    ECSEM (λ _ δ b _ Φx _, storeCT_sem δ b (Φx ())) _.
-  Next Obligation.
-    move=>/= ??*? b ?*?? eqv ?*. case b=>/=; f_equiv; apply eqv.
-  Qed.
+    ECSEM (λ _ δ _ _ Φx _, store δ (Φx ())) _.
+  Next Obligation. solve_proper. Qed.
 End storeC.
 (** [storeC] semantics registered *)
 Notation storeCS := (inCS storeCT).
 
 (** Reify into formulas *)
 Section storeC.
-  Context `{!Csem CON JUDG Σ, !Jsem JUDG (iProp Σ), !storeGS (cifOF CON) Σ,
+  Context `{!Csem CON JUDG Σ, !Jsem JUDG (iProp Σ), !dinvGS (cifOF CON) Σ,
     !storeC CON, !storeJ (cifO CON Σ) JUDG, !storeCS CON JUDG Σ,
     !storeJS (cifOF CON) JUDG Σ}.
 
   #[export] Program Instance store_as_cif {Px} :
     AsCif CON (λ δ, store δ Px) := AS_CIF (cif_store Px) _.
-  Next Obligation. move=>/= *. by rewrite sem_cif_in. Qed.
-  #[export] Program Instance pstore_as_cif {Px} :
-    AsCif CON (λ δ, pstore δ Px) := AS_CIF (cif_pstore Px) _.
   Next Obligation. move=>/= *. by rewrite sem_cif_in. Qed.
 End storeC.
