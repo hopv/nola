d725133ca8c49979f54592d1c7ddd233b2280b58
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-11-10 11:52:42 +0900

    Tweak on ty_uninit

--- nola/examples/rust_halt/uninit.v
+++ nola/examples/rust_halt/uninit.v
@@ -6,7 +6,7 @@ Section uninit.
   Context `{!rust_haltGS CON Σ, !rust_haltC CON, !Csem CON JUDG Σ,
     !Jsem JUDG (iProp Σ), !rust_haltJ CON JUDG Σ, !rust_haltCS CON JUDG Σ}.
 
-  (** ** [ty_uninit]: Universal type *)
+  (** [ty_uninit]: Uninitialized data type *)
   Definition pty_uninit sz : pty CON Σ unitₓ :=
     pair sz (λ _ vl, ⌜length vl = sz⌝%cif).
   Definition ty_uninit sz : ty CON Σ unitₓ := ty_pty (pty_uninit sz).
@@ -14,3 +14,5 @@ Section uninit.
   #[export] Instance pty_uninit_pty {sz} : Pty (pty_uninit sz).
   Proof. split=>//= *. exact _. Qed.
 End uninit.
+(** [ty_unit]: Unit type *)
+Notation ty_unit := (ty_uninit 0).
