1b46da417b5abf8ef7f979c6eff1c860e3c9d9dc
Yusuke Matsushita <ysk.m24t@gmail.com>
2024-11-13 01:34:18 +0900

    Tweak on type_path and type_path_frozen tactics

--- nola/examples/rust_halt/core.v
+++ nola/examples/rust_halt/core.v
@@ -302,7 +302,11 @@ Tactic Notation "type_bind" open_constr(e) :=
   type_reshape e ltac:(fun K => iApply (type_bind K e)).
 Tactic Notation "type_bind" open_constr(e) "with" open_constr(H) :=
   type_reshape e ltac:(fun K => iApply (type_bind K e with H)).
-Tactic Notation "type_path" open_constr(p) :=
-  type_reshape p ltac:(fun K => iApply (type_path_bind K p)).
-Tactic Notation "type_path_frozen" open_constr(p) :=
-  type_reshape p ltac:(fun K => iApply (type_path_frozen_bind K p)).
+Tactic Notation "type_path"
+  open_constr(p) "as" "(" simple_intropattern(v) ")" :=
+  type_reshape p ltac:(fun K =>
+    iApply (type_path_bind K p)=>/=; iIntros (v)).
+Tactic Notation "type_path_frozen"
+  open_constr(p) "as" "(" simple_intropattern(v) ")" :=
+  type_reshape p ltac:(fun K =>
+    iApply (type_path_frozen_bind K p)=>/=; iIntros (v)).
