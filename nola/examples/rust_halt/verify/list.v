a31a671038883d09665525e09f01a4171fb596fd
Yusuke Matsushita <ysk.m24t@gmail.com>
2025-03-16 00:34:38 +0900

    Minor tweak on examples.rust_halt.verify.list

--- nola/examples/rust_halt/verify/list.v
+++ nola/examples/rust_halt/verify/list.v
@@ -8,21 +8,21 @@ Section list.
     !rust_haltJS CON JUDG Σ}.
 
   (** [list_unwrap]: Unwrap function for the list type *)
-  Definition list_unwrap {A} (al : list A) : unit + A *' list A :=
-    match al with [] => inl () | a :: al' => inr (a, al')' end.
+  Definition list_unwrap {A} (l : list A) : unit + A *' list A :=
+    match l with [] => inl () | a :: l' => inr (a, l')' end.
   (** [list_unwrap] is injective *)
-  #[export] Instance list_unwrap_inj {X} : Inj (=) (=) (@list_unwrap X).
+  #[export] Instance list_unwrap_inj {A} : Inj (=) (=) (@list_unwrap A).
   Proof. by move=> [|??][|??]//=[<-<-]. Qed.
 
   (** [list_wrap]: Wrap function for the list type *)
   Definition list_wrap {A} (s : unit + A *' list A) : list A :=
-    match s with inl () => [] | inr (a, al')' => a :: al' end.
+    match s with inl () => [] | inr (a, l')' => a :: l' end.
 
   (** [list_wrap] and [list_unwrap] are mutually inverse *)
-  Lemma list_wrap_unwrap {A xl} : @list_wrap A (list_unwrap xl) = xl.
-  Proof. by case xl. Qed.
+  Lemma list_wrap_unwrap {A l} : @list_wrap A (list_unwrap l) = l.
+  Proof. by case l. Qed.
   Lemma list_unwrap_wrap {A s} : @list_unwrap A (list_wrap s) = s.
-  Proof. by case s=> [[]|[??]]. Qed.
+  Proof. by case s=> [[]|?]. Qed.
 
   (** [ty_list_gen]: Generator of the list type *)
   Definition ty_list_gen' {X} (T : ty CON Σ X) (Self : ty CON Σ (listₓ X))
